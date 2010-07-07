b056863f0d40584e475ce1fed0adc709e67bec24
lamikae <mikael.lammentausta@gmail.com>
2010-07-07 17:14:53 +0300

    use Rails 2.3.8 in the example and by default disable ActiveRecord

--- example/config/environment.rb
+++ example/config/environment.rb
@@ -6,7 +6,7 @@
 #
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
 
   # Skip frameworks you're not going to use. To use Rails without a database
   # you must remove the Active Record framework.
-  config.frameworks -= [:active_record, :active_resource, :action_mailer ]
+  #config.frameworks -= [:active_record, :active_resource, :action_mailer ]
 
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
